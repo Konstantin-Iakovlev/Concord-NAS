Batch normalization fix
Added batch normalization for each domain

hyperDARTS.py
@@ -43,7 +43,7 @@ class HyperDartsLayerChoice(DartsLayerChoice):
             batch: PCA representation (domain is unknown) or index of domain 
             lam: regularization coefficient
         '''
-        op_results = torch.stack([op(inputs) for op in self.op_choices.values()])
+        op_results = torch.stack([op(inputs, domain_idx=batch) for op in self.op_choices.values()])
         # rbf_outputs = self.rbf_net(batch)
         rbf_outputs = self.alpha[batch].unsqueeze(0)
         if self.sampling_mode == 'gumbel-softmax':
@@ -276,7 +276,7 @@ class HyperDartsTrainer(DartsTrainer):
                     metrics['loss'] = loss.item() / self.p[domain_idx]
                     val_meters.update(metrics)
                     # update p[domain_idx]
-                    self.p[domain_idx] *= torch.exp(loss / self.p[domain_idx] * 0.1)
+                    self.p[domain_idx] *= torch.exp(loss / self.p[domain_idx] * 0.01)
                     
 
                 if self.log_frequency is not None and step % self.log_frequency == 0:

model.py
@@ -38,7 +38,8 @@ class AuxiliaryHead(nn.Module):
 
 
 class Node(nn.Module):
-    def __init__(self, node_id, num_prev_nodes, channels, num_downsample_connect):
+    def __init__(self, node_id, num_prev_nodes, channels, num_downsample_connect,
+            num_domains):
         super().__init__()
         self.ops = nn.ModuleList()
         choice_keys = []
@@ -47,13 +48,14 @@ class Node(nn.Module):
             choice_keys.append("{}_p{}".format(node_id, i))
             self.ops.append(
                 LayerChoice(OrderedDict([
-                    ("maxpool", ops.PoolBN('max', channels, 3, stride, 1, affine=False)),
-                    ("avgpool", ops.PoolBN('avg', channels, 3, stride, 1, affine=False)),
-                    ("skipconnect", nn.Identity() if stride == 1 else ops.FactorizedReduce(channels, channels, affine=False)),
-                    ("sepconv3x3", ops.SepConv(channels, channels, 3, stride, 1, affine=False)),
-                    ("sepconv5x5", ops.SepConv(channels, channels, 5, stride, 2, affine=False)),
-                    ("dilconv3x3", ops.DilConv(channels, channels, 3, stride, 2, 2, affine=False)),
-                    ("dilconv5x5", ops.DilConv(channels, channels, 5, stride, 4, 2, affine=False))
+                    ("maxpool", ops.PoolBN('max', channels, 3, stride, 1, affine=False, num_domains=num_domains)),
+                    ("avgpool", ops.PoolBN('avg', channels, 3, stride, 1, affine=False, num_domains=num_domains)),
+                    ("skipconnect", ops.Identity() if stride == 1 else ops.FactorizedReduce(channels,\
+                            channels, affine=False, num_domains=num_domains)),
+                    ("sepconv3x3", ops.SepConv(channels, channels, 3, stride, 1, affine=False, num_domains=num_domains)),
+                    ("sepconv5x5", ops.SepConv(channels, channels, 5, stride, 2, affine=False, num_domains=num_domains)),
+                    ("dilconv3x3", ops.DilConv(channels, channels, 3, stride, 2, 2, affine=False, num_domains=num_domains)),
+                    ("dilconv5x5", ops.DilConv(channels, channels, 5, stride, 4, 2, affine=False, num_domains=num_domains))
                 ]), label=choice_keys[-1]))
         self.drop_path = ops.DropPath()
         self.input_switch = InputChoice(n_candidates=len(choice_keys), n_chosen=2, label="{}_switch".format(node_id))
@@ -85,7 +87,8 @@ class Node(nn.Module):
 
 class Cell(nn.Module):
 
-    def __init__(self, n_nodes, channels_pp, channels_p, channels, reduction_p, reduction):
+    def __init__(self, n_nodes, channels_pp, channels_p, channels, reduction_p, reduction,
+            num_domains):
         super().__init__()
         self.reduction = reduction
         self.n_nodes = n_nodes
@@ -93,16 +96,19 @@ class Cell(nn.Module):
         # If previous cell is reduction cell, current input size does not match with
         # output size of cell[k-2]. So the output[k-2] should be reduced by preprocessing.
         if reduction_p:
-            self.preproc0 = ops.FactorizedReduce(channels_pp, channels, affine=False)
+            self.preproc0 = ops.FactorizedReduce(channels_pp, channels, affine=False,\
+                    num_domains=num_domains)
         else:
-            self.preproc0 = ops.StdConv(channels_pp, channels, 1, 1, 0, affine=False)
-        self.preproc1 = ops.StdConv(channels_p, channels, 1, 1, 0, affine=False)
+            self.preproc0 = ops.StdConv(channels_pp, channels, 1, 1, 0, affine=False,\
+                    num_domains=num_domains)
+        self.preproc1 = ops.StdConv(channels_p, channels, 1, 1, 0, affine=False,\
+                num_domains=num_domains)
 
         # generate dag
         self.mutable_ops = nn.ModuleList()
         for depth in range(2, self.n_nodes + 2):
             self.mutable_ops.append(Node("{}_n{}".format("reduce" if reduction else "normal", depth),
-                                         depth, channels, 2 if reduction else 0))
+                                         depth, channels, 2 if reduction else 0, num_domains))
 
     def forward(self, s0, s1, batch, lam=torch.tensor(0.0)):
         # s0, s1 are the outputs of previous previous cell and previous cell, respectively.
@@ -152,7 +158,7 @@ class CNN(nn.Module):
                 c_cur *= 2
                 reduction = True
 
-            cell = Cell(n_nodes, channels_pp, channels_p, c_cur, reduction_p, reduction)
+            cell = Cell(n_nodes, channels_pp, channels_p, c_cur, reduction_p, reduction, n_heads)
             self.cells.append(cell)
             c_cur_out = c_cur * n_nodes
             channels_pp, channels_p = channels_p, c_cur_out
@@ -163,7 +169,7 @@ class CNN(nn.Module):
                 # self.aux_head = AuxiliaryHead(input_size // 4, channels_p, n_classes)
 
         self.gap = nn.AdaptiveAvgPool2d(1)
-        self.linear = nn.Linear(channels_p, n_classes)
+        self.linear = nn.ModuleList([nn.Linear(channels_p, n_classes) for _ in range(n_heads)])
 
     def forward(self, x, batch, lam=torch.tensor(0.0)):
         s0 = s1 = self.stem(x)
@@ -177,7 +183,7 @@ class CNN(nn.Module):
 
         out = self.gap(s1)
         out = out.view(out.size(0), -1)  # flatten
-        logits = self.linear(out)
+        logits = self.linear[batch](out)
 
         if aux_logits is not None:
             return logits, aux_logits

ops.py
@@ -27,12 +27,23 @@ class DropPath(nn.Module):
 
         return x
 
+class Identity(nn.Module):
+    """
+    Identity operation
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, domain_idx=0):
+        return x
+
 
 class PoolBN(nn.Module):
     """
     AvgPool or MaxPool with BN. `pool_type` must be `max` or `avg`.
     """
-    def __init__(self, pool_type, C, kernel_size, stride, padding, affine=True):
+    def __init__(self, pool_type, C, kernel_size, stride, padding, affine=True,
+            num_domains=1):
         super().__init__()
         if pool_type.lower() == 'max':
             self.pool = nn.MaxPool2d(kernel_size, stride, padding)
@@ -41,11 +52,11 @@ class PoolBN(nn.Module):
         else:
             raise ValueError()
 
-        self.bn = nn.BatchNorm2d(C, affine=affine)
+        self.bn = nn.ModuleList([nn.BatchNorm2d(C, affine=affine) for _ in range(num_domains)])
 
-    def forward(self, x):
+    def forward(self, x, domain_idx=0):
         out = self.pool(x)
-        out = self.bn(out)
+        out = self.bn[domain_idx](out)
         return out
 
 
@@ -53,33 +64,35 @@ class StdConv(nn.Module):
     """
     Standard conv: ReLU - Conv - BN
     """
-    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
+    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True,
+            num_domains=1):
         super().__init__()
         self.net = nn.Sequential(
             nn.ReLU(),
             nn.Conv2d(C_in, C_out, kernel_size, stride, padding, bias=False),
-            nn.BatchNorm2d(C_out, affine=affine)
         )
+        self.bn = nn.ModuleList([nn.BatchNorm2d(C_out, affine=affine) for _ in range(num_domains)])
 
-    def forward(self, x):
-        return self.net(x)
+    def forward(self, x, domain_idx=0):
+        return self.bn[domain_idx](self.net(x))
 
 
 class FacConv(nn.Module):
     """
     Factorized conv: ReLU - Conv(Kx1) - Conv(1xK) - BN
     """
-    def __init__(self, C_in, C_out, kernel_length, stride, padding, affine=True):
+    def __init__(self, C_in, C_out, kernel_length, stride, padding, affine=True,
+            num_domains=1):
         super().__init__()
         self.net = nn.Sequential(
             nn.ReLU(),
             nn.Conv2d(C_in, C_in, (kernel_length, 1), stride, padding, bias=False),
             nn.Conv2d(C_in, C_out, (1, kernel_length), stride, padding, bias=False),
-            nn.BatchNorm2d(C_out, affine=affine)
         )
+        self.bn = nn.ModuleList([nn.BatchNorm2d(C_out, affine=affine) for _ in range(num_domains)])
 
-    def forward(self, x):
-        return self.net(x)
+    def forward(self, x, domain_idx=0):
+        return self.bn[domain_idx](self.net(x))
 
 
 class DilConv(nn.Module):
@@ -88,18 +101,19 @@ class DilConv(nn.Module):
     ReLU - (Dilated) depthwise separable - Pointwise - BN.
     If dilation == 2, 3x3 conv => 5x5 receptive field, 5x5 conv => 9x9 receptive field.
     """
-    def __init__(self, C_in, C_out, kernel_size, stride, padding, dilation, affine=True):
+    def __init__(self, C_in, C_out, kernel_size, stride, padding, dilation, affine=True,
+            num_domains=1):
         super().__init__()
         self.net = nn.Sequential(
             nn.ReLU(),
             nn.Conv2d(C_in, C_in, kernel_size, stride, padding, dilation=dilation, groups=C_in,
                       bias=False),
             nn.Conv2d(C_in, C_out, 1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(C_out, affine=affine)
         )
+        self.bn = nn.ModuleList([nn.BatchNorm2d(C_out, affine=affine) for _ in range(num_domains)])
 
-    def forward(self, x):
-        return self.net(x)
+    def forward(self, x, domain_idx=0):
+        return self.bn[domain_idx](self.net(x))
 
 
 class SepConv(nn.Module):
@@ -107,30 +121,31 @@ class SepConv(nn.Module):
     Depthwise separable conv.
     DilConv(dilation=1) * 2.
     """
-    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True):
+    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine=True,
+            num_domains=1):
         super().__init__()
-        self.net = nn.Sequential(
-            DilConv(C_in, C_in, kernel_size, stride, padding, dilation=1, affine=affine),
-            DilConv(C_in, C_out, kernel_size, 1, padding, dilation=1, affine=affine)
-        )
+        self.conv_1 = DilConv(C_in, C_in, kernel_size, stride, padding, dilation=1, affine=affine,
+            num_domains=num_domains)
+        self.conv_2 = DilConv(C_in, C_out, kernel_size, 1, padding, dilation=1, affine=affine,
+            num_domains=num_domains)
 
-    def forward(self, x):
-        return self.net(x)
+    def forward(self, x, domain_idx=0):
+        return self.conv_2(self.conv_1(x, domain_idx=domain_idx), domain_idx=domain_idx)
 
 
 class FactorizedReduce(nn.Module):
     """
     Reduce feature map size by factorized pointwise (stride=2).
     """
-    def __init__(self, C_in, C_out, affine=True):
+    def __init__(self, C_in, C_out, affine=True, num_domains=1):
         super().__init__()
         self.relu = nn.ReLU()
         self.conv1 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
         self.conv2 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
-        self.bn = nn.BatchNorm2d(C_out, affine=affine)
+        self.bn = nn.ModuleList([nn.BatchNorm2d(C_out, affine=affine) for _ in range(num_domains)])
 
-    def forward(self, x):
+    def forward(self, x, domain_idx=0):
         x = self.relu(x)
         out = torch.cat([self.conv1(x), self.conv2(x[:, :, 1:, 1:])], dim=1)
-        out = self.bn(out)
+        out = self.bn[domain_idx](out)
         return out